2b8ee88380a74415a950cbd647c8a4d2543bb3e6
maximvita223 <109244209+maximvita223@users.noreply.github.com>
2022-12-19 13:40:11 +0300

    fixed

--- WriterTxt.java
+++ WriterTxt.java
@@ -1,4 +1,5 @@
 package DzOOP2;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
@@ -9,20 +10,21 @@ public class WriterTxt implements WriterInFile {
 
     @Override
     public void writer(Map<String, Float> map) {
-        try{
-            File fileTwo=new File("WriteTXT.txt");
-            FileOutputStream fos=new FileOutputStream(fileTwo);
-                PrintWriter pw=new PrintWriter(fos);
-        
-                for(Entry<String, Float> m :map.entrySet()){
-                    pw.println(m.getKey()+"="+m.getValue());
-                }
-        
-                pw.flush();
-                pw.close();
-                fos.close();
-            }catch(Exception e){}
-        
+        try {
+            File file = new File("WriteTXT.txt");
+            FileOutputStream fos = new FileOutputStream(file);
+            PrintWriter pw = new PrintWriter(fos);
+
+            for (Entry<String, Float> element : map.entrySet()) {
+                pw.println(element.getKey() + "=" + element.getValue());
+            }
+
+            pw.flush();
+            pw.close();
+            fos.close();
+        } catch (Exception e) {
+        }
+
     }
-    
+
 }
